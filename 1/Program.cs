149833491a24ade91917a5d7e2be82a549fffb36
Jori Huisman <jori@huisman.io>
2020-12-03 07:57:03 +0100

    Day 1: Report Repair

--- 1/Program.cs
+++ 1/Program.cs
@@ -2,8 +2,18 @@
 using System.IO;
 using System.Linq;
 
+// Part one
+//var ExpenseReport = from Expense in File.ReadAllLines("Input.txt") select int.Parse(Expense);
+//var Matches = from x in ExpenseReport
+//              where ExpenseReport.Any(y => x + y == 2020)
+//              select x;
+//Console.WriteLine(Matches.ElementAt(0) * Matches.ElementAt(1));
+
+// Part two
 var ExpenseReport = from Expense in File.ReadAllLines("Input.txt") select int.Parse(Expense);
 var Matches = from x in ExpenseReport
-              where ExpenseReport.Any(y => x + y == 2020)
-              select x;
-Console.WriteLine(Matches.ElementAt(0) * Matches.ElementAt(1));
+              from y in ExpenseReport
+              from z in ExpenseReport
+              where x + y + z == 2020
+              select x * y * z;
+Console.WriteLine(Matches.Distinct().Single());
