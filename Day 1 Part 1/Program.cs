de92d57fce4d3c739b5d9997b37b77c294d864cf
Jori Huisman <jori@huisman.io>
2020-12-03 08:00:21 +0100

    Day 1 refactor

---
+++ Day 1 Part 1/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var ExpenseReport = from Expense in File.ReadAllLines("Input.txt") select int.Parse(Expense);
+var Matches = from x in ExpenseReport
+              where ExpenseReport.Any(y => x + y == 2020)
+              select x;
+Console.WriteLine(Matches.ElementAt(0) * Matches.ElementAt(1));
--- Day 1 Part 2/Program.cs
+++ Day 1 Part 2/Program.cs
@@ -2,14 +2,6 @@
 using System.IO;
 using System.Linq;
 
-// Part one
-//var ExpenseReport = from Expense in File.ReadAllLines("Input.txt") select int.Parse(Expense);
-//var Matches = from x in ExpenseReport
-//              where ExpenseReport.Any(y => x + y == 2020)
-//              select x;
-//Console.WriteLine(Matches.ElementAt(0) * Matches.ElementAt(1));
-
-// Part two
 var ExpenseReport = from Expense in File.ReadAllLines("Input.txt") select int.Parse(Expense);
 var Matches = from x in ExpenseReport
               from y in ExpenseReport
