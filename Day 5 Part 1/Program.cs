1e291dcc904b6b21472669ccc98b025913271cd1
Jori Huisman <jori@huisman.io>
2020-12-05 14:54:11 +0100

    Cleanup

--- Day 5 Part 1/Program.cs
+++ Day 5 Part 1/Program.cs
@@ -2,6 +2,12 @@
 using System.IO;
 using System.Linq;
 
-var BoardingPassIDs = File.ReadAllLines("Input.txt").Select(BoardingPass => BoardingPass.Select((Row, Partition) => Row == 'B' | Row == 'R' ? 1024 >> (Partition + 1) : 0).Sum());
+var BoardingPassIDs = File.ReadAllLines("Input.txt")
+    .Select(BoardingPass => BoardingPass
+        .Select((Row, Partition) =>
+            Row == 'B' | Row == 'R'
+            ? 1024 >> (Partition + 1)
+            : 0
+        ).Sum());
 
 Console.WriteLine(BoardingPassIDs.Max());
