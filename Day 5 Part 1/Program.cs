8b279b53925bab3d8af70c514d654a98016feb9e
Jori Huisman <jori@huisman.io>
2020-12-05 14:30:55 +0100

    Day 5: Binary Boarding

---
+++ Day 5 Part 1/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var BoardingPassRanks = File.ReadAllLines("Input.txt").Select(BoardingPass => BoardingPass.Select((Row, Partition) => Row == 'B' | Row == 'R' ? 1024 >> (Partition + 1) : 0).Sum());
+
+Console.WriteLine(BoardingPassRanks.Max());
