3194b245752dde14d9e5976538427a6c8c997c56
Jori Huisman <jori@huisman.io>
2020-12-05 14:48:18 +0100

    Day 5: Binary Boarding

--- Day 5 Part 2/Program.cs
+++ Day 5 Part 2/Program.cs
@@ -4,4 +4,6 @@ using System.Linq;
 
 var BoardingPassIDs = File.ReadAllLines("Input.txt").Select(BoardingPass => BoardingPass.Select((Row, Partition) => Row == 'B' | Row == 'R' ? 1024 >> (Partition + 1) : 0).Sum());
 
-Console.WriteLine(BoardingPassIDs.Max());
+var MySeat = BoardingPassIDs.OrderBy(x => x).Where(x => !BoardingPassIDs.Contains(x + 1)).First() + 1;
+
+Console.WriteLine(MySeat);
